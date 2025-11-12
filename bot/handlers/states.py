4ea18cf7600c8c70ed944f0b45b0345bcc304534
holigany47-user <holigany47@gmail.com>
2025-11-13 00:03:05 +0300

    Update states.py

--- bot/handlers/states.py
+++ bot/handlers/states.py
@@ -1,5 +1,5 @@
 from aiogram.fsm.state import State, StatesGroup
 
 class TableStates(StatesGroup):
-    waiting_table_file = State()
-    waiting_update_file = State()
+    waiting_table_file = State()      # для сохранения новых таблиц
+    waiting_update_file = State()     # для обновления существующих таблиц
